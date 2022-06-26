bfcdf10061dcf04c962a31f06108ae5b4d7f6d9d
whaim <whaim1986@gmail.com>
2022-06-26 15:02:56 +0800

    cmake file adjust

---
+++ VulkanDemo/BuildConfig.cmake
@@ -0,0 +1,33 @@
+message(STATUS "custom find vulkan")
+
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    message(STATUS "Configuring on/for Linux")
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+    message(STATUS "Configuring on/for macOS")
+
+    # 包含目录
+    include_directories(/usr/local/Cellar/glfw/3.3.7/include)
+    include_directories(/usr/local/Cellar/glm/0.9.9.8/include)
+    include_directories(/Users/whaim/VulkanSDK/1.3.216.0/macOS/include)
+
+    # 库目录
+    link_directories(/usr/local/Cellar/glfw/3.3.7/lib)
+    link_directories(/Users/whaim/VulkanSDK/1.3.216.0/macOS/lib)
+
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    message(STATUS "Configuring on/for Windows")
+
+    # 包含目录
+    include_directories(D:/VulkanSDK/1.3.216.0/Include)
+    include_directories("C:/Program Files (x86)/GLFW/include")
+    include_directories(D:/VulkanSDK/glm-0.9.9.8/glm)
+    include_directories(D:/VulkanSDK/tinyobjloader-1.0.6)
+    # 库目录
+    link_directories(D:/vulkanSDK/1.3.216.0/Lib)
+    link_directories("C:/Program Files (x86)/GLFW/lib")
+
+elseif(CMAKE_SYSTEM_NAME STREQUAL "AIX")
+    message(STATUS "Configuring on/for IBM AIX")
+else()
+    message(STATUS "Configuring on/for ${CMAKE_SYSTEM_NAME}")
+endif()
